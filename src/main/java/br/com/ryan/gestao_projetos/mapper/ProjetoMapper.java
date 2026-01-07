9758f31220e0397c3e7ad2d2610e1b4922ca474f
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-01-07 02:23:09 -0300

    criacao de mais metodos nos mappers

--- src/main/java/br/com/ryan/gestao_projetos/mapper/ProjetoMapper.java
+++ src/main/java/br/com/ryan/gestao_projetos/mapper/ProjetoMapper.java
@@ -1,14 +1,37 @@
 package br.com.ryan.gestao_projetos.mapper;
 
+import br.com.ryan.gestao_projetos.dto.ProjetoRequest;
 import br.com.ryan.gestao_projetos.dto.ProjetoResponse;
 import br.com.ryan.gestao_projetos.dto.TarefaResponse;
 import br.com.ryan.gestao_projetos.model.Projeto;
+import br.com.ryan.gestao_projetos.model.Tarefa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class ProjetoMapper {
-    public static ProjetoResponse toProjetoResponse(Projeto projeto) {
-        List<TarefaResponse> tarefas = projeto.getTarefas().stream().map(t -> TarefasMapper.toTarefaResponse(t)).toList();
+
+    @Autowired
+    private TarefasMapper tarefasMapper;
+
+    public Projeto toProjeto(ProjetoRequest projetoRequest) {
+        Projeto projeto = new Projeto();
+        projeto.setNome(projetoRequest.nome());
+        projeto.setDescricao(projetoRequest.descricao());
+        projeto.setDataInicio(projetoRequest.dataInicio());
+        List<Tarefa> tarefas = projetoRequest.tarefas().stream().map(t -> {
+            Tarefa tarefa = tarefasMapper.toTarefa(t);
+            tarefa.setProjeto(projeto);
+            return tarefa;
+        }).toList();
+        projeto.setTarefas(tarefas);
+        return projeto;
+    }
+
+    public ProjetoResponse toProjetoResponse(Projeto projeto) {
+        List<TarefaResponse> tarefas = projeto.getTarefas().stream().map(t -> tarefasMapper.toTarefaResponse(t)).toList();
         return new ProjetoResponse(projeto.getId(), projeto.getNome(), projeto.getDescricao(), projeto.getDataInicio(), tarefas);
     }
 }
--- src/main/java/br/com/ryan/gestao_projetos/mapper/TarefasMapper.java
+++ src/main/java/br/com/ryan/gestao_projetos/mapper/TarefasMapper.java
@@ -1,10 +1,21 @@
 package br.com.ryan.gestao_projetos.mapper;
 
+import br.com.ryan.gestao_projetos.dto.TarefaRequest;
 import br.com.ryan.gestao_projetos.dto.TarefaResponse;
 import br.com.ryan.gestao_projetos.model.Tarefa;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TarefasMapper {
-    public static TarefaResponse toTarefaResponse(Tarefa tarefa) {
+    public Tarefa toTarefa(TarefaRequest tarefaRequest) {
+        Tarefa tarefa = new Tarefa();
+        tarefa.setTitulo(tarefaRequest.titulo());
+        tarefa.setStatus(tarefaRequest.status());
+        tarefa.setPrazo(tarefaRequest.prazo());
+        return tarefa;
+    }
+
+    public TarefaResponse toTarefaResponse(Tarefa tarefa) {
         return new TarefaResponse(tarefa.getId(), tarefa.getTitulo(), tarefa.getStatus(), tarefa.getPrazo(),tarefa.getProjeto().getId());
     }
 }
