32f62376d7019ff37b6ae97a19e50cb33e7d486e
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-01-07 03:28:15 -0300

    criacao dos controllers

---
+++ src/main/java/br/com/ryan/gestao_projetos/controller/ProjetoController.java
@@ -0,0 +1,47 @@
+package br.com.ryan.gestao_projetos.controller;
+
+import br.com.ryan.gestao_projetos.dto.ProjetoRequest;
+import br.com.ryan.gestao_projetos.dto.ProjetoResponse;
+import br.com.ryan.gestao_projetos.dto.TarefaResponse;
+import br.com.ryan.gestao_projetos.model.Projeto;
+import br.com.ryan.gestao_projetos.service.ProjetoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/projetos")
+public class ProjetoController {
+    @Autowired
+    private ProjetoService projetoService;
+
+    @PostMapping
+    public ResponseEntity<Projeto> criarProjeto(@RequestBody ProjetoRequest projetoRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(projetoService.criarProjeto(projetoRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProjetoResponse>> listarProjetos(){
+        return ResponseEntity.ok(projetoService.ListarProjetos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<TarefaResponse>> buscarTarefas(@PathVariable Long id){
+        return ResponseEntity.ok(projetoService.listarTarefasPorProjeto(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProjetoResponse> atualizarProjeto(@PathVariable Long id, @RequestBody ProjetoRequest projetoRequest){
+        return ResponseEntity.ok(projetoService.atualizarProjeto(id, projetoRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProjeto(@PathVariable Long id){
+        projetoService.deletarProjeto(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/br/com/ryan/gestao_projetos/controller/TarefaController.java
@@ -0,0 +1,22 @@
+package br.com.ryan.gestao_projetos.controller;
+
+import br.com.ryan.gestao_projetos.dto.TarefaRequest;
+import br.com.ryan.gestao_projetos.model.Tarefa;
+import br.com.ryan.gestao_projetos.service.TarefaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tarefas")
+public class TarefaController {
+
+    @Autowired
+    private TarefaService tarefaService;
+
+    @PostMapping()
+    public ResponseEntity<Tarefa> save(@RequestBody TarefaRequest tarefaRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(tarefaService.AdicionarTarefa(tarefaRequest));
+    }
+}
