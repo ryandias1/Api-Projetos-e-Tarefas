bcfe9d484bc42e37bc3c4050161fbbb893c9a6fc
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-01-07 11:40:42 -0300

    correcao de verificações e uso de dto nos services

--- src/main/java/br/com/ryan/gestao_projetos/service/ProjetoService.java
+++ src/main/java/br/com/ryan/gestao_projetos/service/ProjetoService.java
@@ -1,5 +1,6 @@
 package br.com.ryan.gestao_projetos.service;
 
+import br.com.ryan.gestao_projetos.dto.ProjetoAtualizarRequest;
 import br.com.ryan.gestao_projetos.dto.ProjetoRequest;
 import br.com.ryan.gestao_projetos.dto.ProjetoResponse;
 import br.com.ryan.gestao_projetos.dto.TarefaResponse;
@@ -7,9 +8,12 @@ import br.com.ryan.gestao_projetos.mapper.ProjetoMapper;
 import br.com.ryan.gestao_projetos.mapper.TarefasMapper;
 import br.com.ryan.gestao_projetos.model.Projeto;
 import br.com.ryan.gestao_projetos.repository.ProjetoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -24,10 +28,10 @@ public class ProjetoService {
     @Autowired
     private TarefasMapper tarefaMapper;
 
-    public Projeto criarProjeto(ProjetoRequest projetoRequest) {
-        if (projetoRequest.nome().isBlank()) throw new IllegalArgumentException("Nome do projeto é obrigatório");
+    public ProjetoResponse criarProjeto(ProjetoRequest projetoRequest) {
+        if (projetoRequest.dataInicio().isBefore(LocalDate.now())) throw new IllegalArgumentException("Data de inicio invalida");
         Projeto projeto = projetoMapper.toProjeto(projetoRequest);
-        return projetoRepository.save(projeto);
+        return projetoMapper.toProjetoResponse(projetoRepository.save(projeto));
     }
 
     public List<ProjetoResponse> ListarProjetos() {
@@ -40,18 +44,18 @@ public class ProjetoService {
     }
 
     public void deletarProjeto(Long id) {
-        projetoRepository.deleteById(id);
+        if (projetoRepository.existsById(id)) projetoRepository.deleteById(id);
     }
 
-    public ProjetoResponse atualizarProjeto(Long id, ProjetoRequest projetoRequest) {
+    public ProjetoResponse atualizarProjeto(Long id, ProjetoAtualizarRequest projetoRequest) {
         Projeto projeto = buscarProjetoPorId(id);
-        if (!(projetoRequest.nome().isBlank())) projeto.setNome(projetoRequest.nome());
+        if (StringUtils.hasText(projetoRequest.nome())) projeto.setNome(projetoRequest.nome());
         if (!(projetoRequest.descricao().isBlank())) projeto.setDescricao(projetoRequest.descricao());
         if (projetoRequest.dataInicio() != null) projeto.setDataInicio(projetoRequest.dataInicio());
         return projetoMapper.toProjetoResponse(projetoRepository.save(projeto));
     }
 
     public Projeto buscarProjetoPorId(Long id) {
-        return projetoRepository.findById(id).orElseThrow(() -> new RuntimeException("Projeto não existe"));
+        return projetoRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Projeto não existe"));
     }
 }
--- src/main/java/br/com/ryan/gestao_projetos/service/TarefaService.java
+++ src/main/java/br/com/ryan/gestao_projetos/service/TarefaService.java
@@ -1,12 +1,15 @@
 package br.com.ryan.gestao_projetos.service;
 
 import br.com.ryan.gestao_projetos.dto.TarefaRequest;
+import br.com.ryan.gestao_projetos.dto.TarefaResponse;
 import br.com.ryan.gestao_projetos.mapper.TarefasMapper;
 import br.com.ryan.gestao_projetos.model.Projeto;
 import br.com.ryan.gestao_projetos.model.Tarefa;
 import br.com.ryan.gestao_projetos.repository.TarefaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class TarefaService {
@@ -20,11 +23,11 @@ public class TarefaService {
     @Autowired
     private ProjetoService projetoService;
 
-    public Tarefa AdicionarTarefa(TarefaRequest tarefaRequest) {
+    public TarefaResponse AdicionarTarefa(TarefaRequest tarefaRequest) {
         Projeto projeto = projetoService.buscarProjetoPorId(tarefaRequest.projetoId());
-        if (tarefaRequest.titulo().isBlank()) throw new IllegalArgumentException("Titulo é obrigatorio");
+        if (tarefaRequest.prazo().isBefore(LocalDate.now())) throw new IllegalArgumentException("Prazo de tarefa invalido");
         Tarefa tarefa = tarefaMapper.toTarefa(tarefaRequest);
         tarefa.setProjeto(projeto);
-        return tarefaRepository.save(tarefa);
+        return tarefaMapper.toTarefaResponse(tarefaRepository.save(tarefa));
     }
 }
