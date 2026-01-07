63293b5147d6851d7425cec5160f5cc66aed943d
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-01-07 01:15:43 -0300

    commit inicial

---
+++ src/main/java/br/com/ryan/gestao_projetos/GestaoProjetosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ryan.gestao_projetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoProjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ryan/gestao_projetos/GestaoProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ryan.gestao_projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
